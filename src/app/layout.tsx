fb688dde91510cf3359e305158cd303255ffdff2
SeongYoon <namo4862@gmail.com>
2025-03-05 14:21:41 +0900

    feat: react query provider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import QueryProvider from "./provider/QueryProvider";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={``}>{children}</body>
+      <body className={``}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
