429ea210c598178f9f7ff564503291842fed6f2f
SeongYoon <namo4862@gmail.com>
2025-03-05 11:41:07 +0900

    fix: post 부분 에러 수정
    post 조회 중 0건이여도 빈배열이기때문에 처리안되던 것 수정
    dynamic route에 에러 수정

--- src/app/api/post/[sn]/route.ts
+++ src/app/api/post/[sn]/route.ts
@@ -1,6 +1,10 @@
 import prisma from "@/lib/prisma";
+import { NextRequest } from "next/server";
 
-export async function GET({ params }: { params: Promise<{ sn: string }> }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ sn: string }> }
+) {
   const { sn } = await params;
   const post = await prisma.post.findUnique({
     where: {
--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 
 export async function GET() {
   const post = await prisma.post.findMany();
-  if (!post) {
+  if (!post || post.length === 0) {
     return new Response("Not found", { status: 404 });
   }
   return new Response(JSON.stringify(post), {
