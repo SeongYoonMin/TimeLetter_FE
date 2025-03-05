1dbfd542bb416e6d0b21f762212726e5727994dc
SeongYoon <namo4862@gmail.com>
2025-03-05 10:11:21 +0900

    feat: prisma, crypto lib 생성

---
+++ src/lib/crypto.ts
@@ -0,0 +1,29 @@
+import crypto from "crypto";
+
+const algorithm = process.env.CRYPTO_ALGORITHM as string;
+const getHash = process.env.CRYPTO_HASH as string;
+const key = process.env.CRYPTO_KEY as string;
+const iv = process.env.CRYPTO_IV as string;
+const ipEncoding = process.env.CRYPTO_IP_ENCODING as BufferEncoding;
+const opEncoding = process.env.CRYPTO_OP_ENCODING as BufferEncoding;
+const secretKey = crypto.createHash(getHash).update(key).digest();
+
+export const actionEncrypt = async (text: string): Promise<string> => {
+  if (!secretKey || !iv) {
+    return "ERROR";
+  }
+  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+  let encrypted = cipher.update(text, ipEncoding, opEncoding);
+  encrypted += cipher.final(opEncoding);
+  return encrypted;
+};
+
+export const actionDecrypt = async (text: string): Promise<string> => {
+  if (!secretKey || !iv) {
+    return "ERROR";
+  }
+  const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+  let decrypted = decipher.update(text, opEncoding, ipEncoding);
+  decrypted += decipher.final(ipEncoding);
+  return decrypted;
+};
