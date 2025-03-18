6a294bc7e68504609a01231776e38a69794a584b
SeongYoon <namo4862@gmail.com>
2025-03-18 13:26:57 +0900

    chore: props 수정

--- src/hooks/post/usePosterList.ts
+++ src/hooks/post/usePosterList.ts
@@ -1,7 +1,7 @@
 // 편지 리스트를 불러오는 API
 import { useQuery } from "@tanstack/react-query";
 
-export const usePosterList = ({ uniqueId }: { uniqueId: string }) => {
+export const usePosterList = (uniqueId: string) => {
   return useQuery({
     queryKey: ["post", uniqueId],
     queryFn: async () => {
