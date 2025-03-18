f54fe5c2da94472cc03b43cec1a9d4474cf11c9f
SeongYoon <namo4862@gmail.com>
2025-03-18 10:34:07 +0900

    chore: 디렉터리 구조 변경
    초대링크인 post와 사용자가 내역을 조회할 수 있는 letter를 구분하기위함

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import PostLetterContainer from "@/components/letter/PostLetterContainer";
+import PostLetterProvider from "@/providers/PostLetterProvider";
+import React from "react";
 
-const page = () => {
+const Page = () => {
+  // ?userid=26wm3Z5fPu2aIIIo2AzkzQ%3D%3D&nickname=%EC%9C%A0%EB%8B%88%EB%AA%A8%EC%B0%8C&background=%2Fimg%2Fpond.png&capsule=heart
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full bg-[#F7F7F7]">
+      <PostLetterProvider>
+        <PostLetterContainer />
+      </PostLetterProvider>
+    </section>
+  );
+};
 
-export default page
+export default Page;
