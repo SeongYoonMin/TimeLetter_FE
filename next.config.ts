77f1b602703a490db702724954b99b700b1e898e
SeongYoon <namo4862@gmail.com>
2025-04-14 09:39:29 +0900

    build: vercel로 수정하면서 assetPrefix 불필요해짐

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/TimeLetter_FE/" : "",
 };
 
 export default nextConfig;
